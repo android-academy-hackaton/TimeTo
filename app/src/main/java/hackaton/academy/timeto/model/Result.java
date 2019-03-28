c44765a96017222d5d30dd6b062684db7ceb523b
gal zaid <galzaid@gmail.com>
2019-03-28 23:24:07 +0200

    Model

---
+++ app/src/main/java/hackaton/academy/timeto/model/Result.java
@@ -0,0 +1,220 @@
+
+package hackaton.academy.timeto.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result {
+
+    @SerializedName("geometry")
+    @Expose
+    private Geometry geometry;
+    @SerializedName("icon")
+    @Expose
+    private String icon;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("opening_hours")
+    @Expose
+    private OpeningHours openingHours;
+    @SerializedName("photos")
+    @Expose
+    private List<Photo> photos = null;
+    @SerializedName("place_id")
+    @Expose
+    private String placeId;
+    @SerializedName("plus_code")
+    @Expose
+    private PlusCode plusCode;
+    @SerializedName("rating")
+    @Expose
+    private Double rating;
+    @SerializedName("reference")
+    @Expose
+    private String reference;
+    @SerializedName("scope")
+    @Expose
+    private String scope;
+    @SerializedName("types")
+    @Expose
+    private List<String> types = null;
+    @SerializedName("user_ratings_total")
+    @Expose
+    private Integer userRatingsTotal;
+    @SerializedName("vicinity")
+    @Expose
+    private String vicinity;
+    @SerializedName("price_level")
+    @Expose
+    private Integer priceLevel;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Result() {
+    }
+
+    /**
+     * 
+     * @param icon
+     * @param priceLevel
+     * @param scope
+     * @param openingHours
+     * @param plusCode
+     * @param reference
+     * @param geometry
+     * @param id
+     * @param photos
+     * @param vicinity
+     * @param placeId
+     * @param name
+     * @param rating
+     * @param userRatingsTotal
+     * @param types
+     */
+    public Result(Geometry geometry, String icon, String id, String name, OpeningHours openingHours, List<Photo> photos, String placeId, PlusCode plusCode, Double rating, String reference, String scope, List<String> types, Integer userRatingsTotal, String vicinity, Integer priceLevel) {
+        super();
+        this.geometry = geometry;
+        this.icon = icon;
+        this.id = id;
+        this.name = name;
+        this.openingHours = openingHours;
+        this.photos = photos;
+        this.placeId = placeId;
+        this.plusCode = plusCode;
+        this.rating = rating;
+        this.reference = reference;
+        this.scope = scope;
+        this.types = types;
+        this.userRatingsTotal = userRatingsTotal;
+        this.vicinity = vicinity;
+        this.priceLevel = priceLevel;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public PlusCode getPlusCode() {
+        return plusCode;
+    }
+
+    public void setPlusCode(PlusCode plusCode) {
+        this.plusCode = plusCode;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+    public Integer getUserRatingsTotal() {
+        return userRatingsTotal;
+    }
+
+    public void setUserRatingsTotal(Integer userRatingsTotal) {
+        this.userRatingsTotal = userRatingsTotal;
+    }
+
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    public Integer getPriceLevel() {
+        return priceLevel;
+    }
+
+    public void setPriceLevel(Integer priceLevel) {
+        this.priceLevel = priceLevel;
+    }
+
+}
