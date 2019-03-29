6c5c6b02178c1fa89d84fd6c8261abae1f66b5c8
moria <moria@soluto.com>
2019-03-29 07:13:39 +0300

    items in list

--- app/src/main/java/hackaton/academy/timeto/FindContactsActivity.java
+++ app/src/main/java/hackaton/academy/timeto/FindContactsActivity.java
@@ -2,6 +2,7 @@ package hackaton.academy.timeto;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -10,10 +11,14 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -25,6 +30,7 @@ public class FindContactsActivity extends AppCompatActivity {
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     private static final String TAG ="mo" ;
     ListView listview;
+    List<String> data_array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +61,11 @@ public class FindContactsActivity extends AppCompatActivity {
         } else {
             // Android version is lesser than 6.0 or the permission is already granted.
             List<String> contacts = getContactNames();
-
+            data_array = contacts;
             listview = (ListView) findViewById(R.id.listview);
-            listview.setAdapter( new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts));
+            CustomAdapter cus = new CustomAdapter();
+            listview.setAdapter(cus);
+           // listview.setAdapter( new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts));
         }
     }
 
@@ -94,7 +102,7 @@ public class FindContactsActivity extends AppCompatActivity {
                                 ContactsContract.CommonDataKinds.Phone.NUMBER));
                         Log.i(TAG, "Name: " + name);
                         Log.i(TAG, "Phone Number: " + phoneNo);
-                        contacts.add(name + phoneNo);
+                        contacts.add(name);
                     }
 //                    contacts.add(name );
 
@@ -121,4 +129,57 @@ public class FindContactsActivity extends AppCompatActivity {
             }
         }
     }
+
+
+class CustomAdapter extends BaseAdapter {
+    LayoutInflater mInflater;
+
+
+    public CustomAdapter() {
+        mInflater = (LayoutInflater) FindContactsActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return data_array.size();//listview item count.
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        final ViewHolder vh;
+        vh = new ViewHolder();
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.row, parent, false);
+            //inflate custom layour
+            vh.tv2 = (TextView) convertView.findViewById(R.id.textView2);
+
+        } else {
+            convertView.setTag(vh);
+        }
+        //vh.tv2.setText("Position = "+position);
+        vh.tv2.setText(data_array.get(position));
+        //set text of second textview based on position
+
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView tv1, tv2;
+    }
+}
 }
